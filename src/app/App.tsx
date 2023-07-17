4d17ab9b16ddc248d83aa47117a4da3de23c6906
Roman <romantut1988@mail.ru>
2023-07-18 01:47:29 +0300

    project add

---
+++ src/app/App.tsx
@@ -0,0 +1,72 @@
+import React, {useEffect} from 'react'
+import './App.css'
+import {useAppDispatch, useAppSelector} from './store'
+import {RequestStatusType} from './app-reducer'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import LinearProgress from '@mui/material/LinearProgress';
+import {Menu} from '@mui/icons-material';
+import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
+import {Login} from "../features/login/Login";
+import {Routes, Route, Navigate} from 'react-router-dom'
+import {TodolistsList} from "../features/TodolistsList/TodolistsList";
+import {logOutTC, meTC} from "../features/login/authReducer";
+import CircularProgress from '@mui/material/CircularProgress/CircularProgress';
+
+
+function App() {
+    const dispatch = useAppDispatch()
+    const status = useAppSelector<RequestStatusType>((state) => state.app.status)
+    const isInitialized = useAppSelector<boolean>((state) => state.app.isInitialized)
+    const isLoggedIn = useAppSelector<boolean>((state) => state.auth.isLoggedIn)
+
+    const logOutHandler = () => {
+        dispatch(logOutTC())
+    }
+
+    useEffect(() => {
+        console.log('1')
+        dispatch(meTC());
+    }, [])
+
+    if (!isInitialized) {
+        return <div
+            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+            <CircularProgress/>
+        </div>
+    }
+
+    return (
+        <div className="App">
+            <ErrorSnackbar/>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    {isLoggedIn && <Button color="inherit" onClick={logOutHandler}>Log out</Button>}
+                </Toolbar>
+                {status === 'loading' && <LinearProgress/>}
+            </AppBar>
+            <Container fixed>
+                <Routes>
+                    <Route path={"/"} element={<TodolistsList/>}/>
+                    <Route path={"/login"} element={<Login/>}/>
+                    <Route path={"/404"} element={<h1>404: PAGE NOT FOUND</h1>}/>
+                    <Route path="*" element={<Navigate to={'/404'}/>}/>
+                </Routes>
+            </Container>
+        </div>
+    )
+}
+
+//1, 2, 3
+
+export default App
---
+++ src/app/app-reducer.ts
@@ -0,0 +1,40 @@
+const initialState: InitialStateType = {
+    isInitialized: false,
+    status: 'idle',
+    error: null
+}
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
+        case 'APP/SET-INITIALIZED':
+            return {...state, isInitialized: action.isInitialized}
+        default:
+            return {...state}
+    }
+}
+
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+export type InitialStateType = {
+    isInitialized: boolean
+    // происходит ли сейчас взаимодействие с сервером
+    status: RequestStatusType
+    // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
+    error: string | null
+}
+
+export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
+export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setInitializedAC = (isInitialized: boolean) => ({type: 'APP/SET-INITIALIZED', isInitialized} as const)
+
+export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
+export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+export type SetInitializedType = ReturnType<typeof setInitializedAC>
+
+type ActionsType =
+    | SetAppErrorActionType
+    | SetAppStatusActionType
+    | SetInitializedType
