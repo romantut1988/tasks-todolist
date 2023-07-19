f55e28cef98b05158fbef05417a1bb12271d970d
Roman <romantut1988@mail.ru>
2023-07-19 09:29:06 +0300

    restart test project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -59,7 +59,7 @@ function App() {
                 <Routes>
                     <Route path={"/"} element={<TodolistsList/>}/>
                     <Route path={"/login"} element={<Login/>}/>
-                    <Route path={"/404"} element={<h1>404: PAGE NOT FOUND</h1>}/>
+                    <Route path={"/404"} element={<h1>404: PAGE NOT FOUNDS</h1>}/>
                     <Route path="*" element={<Navigate to={'/404'}/>}/>
                 </Routes>
             </Container>
